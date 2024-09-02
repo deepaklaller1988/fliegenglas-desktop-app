85e022c34f3c5fdd552587f3ba7cd2a359f1ff8a
Dlaller <dlaller1988@gmail.com>
2024-09-02 16:57:25 +0530

    Updated Code 02 September

--- src/components/OfflinePage.tsx
+++ src/components/OfflinePage.tsx
@@ -1,18 +1,19 @@
 import useNetworkCheck from '@hooks/useNetworkCheck';
 import React from 'react'
-
+import Image from 'next/image';
 export default function OfflinePage() {
     const { isOnline } = useNetworkCheck();
 
-    const handleRefresh =async () => {
-            if (isOnline) {
-                window.location.reload();
-        };    };
+    const handleRefresh = async () => {
+        if (isOnline) {
+            window.location.reload();
+        };
+    };
     return (
         <div className="flex items-center justify-center h-screen px-5">
             <div className="text-center text-white max-w-xs flex flex-col gap-5 items-center inline-block">
                 <span>
-                    <img src="/assets/images/wifi.svg" alt="wifi-disabled" />
+                    <Image src="/wifi.svg" alt="wifi-disabled" width={100} height={100}  loading="lazy" />
                 </span>
                 <p>Du bist offline oder die Netzwerkverbindung ist schwach.</p>
                 <button className="bg-[#ff9900] text-white p-2 w-full rounded-md" onClick={handleRefresh}>
